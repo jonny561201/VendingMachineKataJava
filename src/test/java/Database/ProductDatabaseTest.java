aaceaddf706f0bb9382a51c8391d27991ac17a35
Jon Graf <jongraf1@gmail.com>
2019-05-16 17:44:48 -0500

    added database tests to ensure we can also delete items from database

--- src/test/java/Database/ProductDatabaseTest.java
+++ src/test/java/Database/ProductDatabaseTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -22,6 +23,7 @@ public class ProductDatabaseTest {
 
     @Autowired
     private ProductDatabase database;
+
     private static final String PRODUCT_LOCATION = "A69";
 
     @After
@@ -31,11 +33,14 @@ public class ProductDatabaseTest {
 
     @Test
     public void getProductsByLocation_ShouldReturnProductsFromTable() {
-        String productLocation = "C3";
-        List<Product> actualProducts = database.getProductsByLocation(productLocation);
+        String expectedName = "Twix";
+        Product product = new Product(expectedName, PRODUCT_LOCATION, BigDecimal.ONE);
+        database.batchInsertProducts(Collections.singletonList(product));
+
+        List<Product> actualProducts = database.getProductsByLocation(PRODUCT_LOCATION);
 
         Product actualProduct = actualProducts.get(0);
-        assertEquals("Twix", actualProduct.getName());
+        assertEquals(expectedName, actualProduct.getName());
     }
 
     @Test
@@ -50,5 +55,25 @@ public class ProductDatabaseTest {
         assertEquals(2, products.size());
         assertEquals(product1.getName(), products.stream().filter(x -> x.getName().equals(product1.getName())).findFirst().get().getName());
         assertEquals(product2.getName(), products.stream().filter(x -> x.getName().equals(product2.getName())).findFirst().get().getName());
+    }
+
+    @Test
+    public void deleteProductsByLocation_ShouldRemoveAllProductsAtALocation() {
+        String name1 = "otherItem";
+        String name2 = "BestItem";
+        Product product1 = new Product(name1, PRODUCT_LOCATION, BigDecimal.ZERO);
+        Product product2 = new Product(name2, PRODUCT_LOCATION, BigDecimal.ZERO);
+
+        database.batchInsertProducts(Arrays.asList(product1, product2));
+
+        List<Product> products = database.getProductsByLocation(PRODUCT_LOCATION);
+        assertEquals(2, products.size());
+        assertEquals(name1, products.stream().filter(x -> x.getName().equals(name1)).findFirst().get().getName());
+        assertEquals(name1, products.stream().filter(x -> x.getName().equals(name1)).findFirst().get().getName());
+
+        database.deleteProductsByLocation(PRODUCT_LOCATION);
+
+        List<Product> expectedProducts = database.getProductsByLocation(PRODUCT_LOCATION);
+        assertEquals(Collections.EMPTY_LIST, expectedProducts);
     }
 }
